92ef4aa899e8f74c3c44adafff1e5c2a26a25999
Lennert Jansen <lennertjansen@Lennerts-MacBook-Pro.local>
2018-06-18 16:55:43 +0200

    sdfghj

--- scripts/atp.js
+++ scripts/atp.js
@@ -134,7 +134,9 @@ window.onload = function() {
 
         playerData = data;
 
-        makeProfile(playerData);
+        //makeProfile(playerData);
+
+        makeTree(playerData);
 
     });
 
@@ -257,14 +259,54 @@ function makeProfile(data){
     // call bar chart function using a player object as an argument
     makeBarChart(data, playerNumber, 2);
 
+    updateBarchart(data);
+
+};
+
+function updateBarchart(data){
+
+    playerNumber = Math.floor(Math.random() * 743);
+
+      // console.log('verververver', data)
+
+      var butWin = document.getElementById("selectWin");
+      var butAce = document.getElementById("selectAce");
+      var butDf = document.getElementById("selectDf");
+
+      // eventhandler > roept linegraph functie aan met de data die je wil hebben
+
+      butWin.addEventListener("click", {
+        handleEvent: function (event){
+          makeBarChart(data, playerNumber, 0)
+        }
+      });
+
+      butAce.addEventListener("click", {
+        handleEvent: function (event){
+          makeBarChart(data, playerNumber, 1)
+        }
+      });
+
+      butDf.addEventListener("click", {
+        handleEvent: function (event) {
+          makeBarChart(data, playerNumber, 2)
+        }
+      });
+
 };
 
 function makeBarChart(data, playerNumber, statistic){
 
+    // // remove current svg's if others are added by clicking on dropdown
+    // if (d3.select("#barchart").select("svg")){
+    //     d3.select("#barchart").select("svg").remove();
+    // };
+
+    d3.select('#barchart').remove();
+
+
     player = data[playerNumber]
     //player = data[213];
-
-
 
     // create list of objects for the dropdown menus
     var selectData = [  { "label" : "Match Winning Rate (%)", "aus" : "win_rate_aus", "rg" : "win_rate_rg", "wim" : "win_rate_wim", "us" : "win_rate_us", "total" : "win_rate" },
@@ -297,8 +339,8 @@ function makeBarChart(data, playerNumber, statistic){
     var formatPercent = d3.format('.2%');
 
     // render svg canvas for bar chart
-    var barSvg = d3.select("body")
-        .append("svg")
+    var barSvg = d3.select("body").append("svg")
+        .attr("id", "barchart")
     	.attr("width", barchartOuterWidth)
         .attr("height", barchartOuterHeight)
     	.append("g")
@@ -437,7 +479,7 @@ function makeBarChart(data, playerNumber, statistic){
     //      return d.label;
     //    })
 
-    makePie(data, "win_rate_wim");
+    //makePie(data, "win_rate_wim");
 
 };
 
@@ -545,7 +587,9 @@ function makePie(data, tournament){
 
 };
 
-function makeTree(data, playerNumber){
+function makeTree(data){
+
+    playerNumber = Math.floor(Math.random() * 743);
 
     player = data[playerNumber];
 
@@ -556,204 +600,211 @@ function makeTree(data, playerNumber){
       {
         "name": "Personal Stats",
         "children": [
-          { "name": player.nationality },
-          { "name": player.height },
-          { "name" : player.hand}
+          { "name": "Nationality: " + player.nationality },
+          { "name": "Height: " + player.height + " cm" },
+          { "name" : "Hand: " + player.hand}
 
         ]
       },
       {
           "name": "Match Stats" ,
           "children" : [
-              { "name" : player.win_rate },
-              { "name" : player.mean_ace},
-              { "name" : player.mean_df}
+              { "name" : "Match Winning Rate: " + round(player.win_rate * 100, 2) + "%" },
+              { "name" : "Average No. of Aces per Match: " + round(player.mean_ace, 2)},
+              { "name" : "Average No. of Double Faults per Match: " + round(player.mean_df, 2)}
           ]
       }
     ]
     };
 
-    // Set the dimensions and margins of the diagram
-var margin = {top: 40, right: 90, bottom: 30, left: 90},
-    width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    // set the dimensions and margins of the diagram
+    var treeMargin = {top: 40, right: 90, bottom: 30, left: 200},
+        treeOuterWidth = 1000;
+        treeOuterHeight = 500;
+        treeWidth = treeOuterWidth - treeMargin.left - treeMargin.right,
+        treeHeight = treeOuterHeight - treeMargin.top - treeMargin.bottom;
 
-// append the svg object to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate("
-          + margin.left + "," + margin.top + ")");
+    // append the svg object to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var treeSvg = d3.select("body").append("svg")
+        .attr("width", treeOuterWidth)
+        .attr("height", treeOuterHeight)
+        .append("g")
+        .attr("transform", "translate("
+              + treeMargin.left + "," + treeMargin.top + ")");
 
-var i = 0,
-    duration = 750,
-    root;
+    var i = 0,
+        duration = 750,
+        root;
 
-// declares a tree layout and assigns the size
-var treemap = d3.tree().size([height, width]);
+    // declares a tree layout and assigns the size
+    var treemap = d3.tree().size([treeHeight, treeWidth]);
 
-// Assigns parent, children, height, depth
-root = d3.hierarchy(treeData, function(d) { return d.children; });
-root.x0 = height / 2;
-root.y0 = 0;
+    // Assigns parent, children, height, depth
+    root = d3.hierarchy(treeData, function(d) { return d.children; });
+    root.x0 = treeHeight / 2;
+    root.y0 = 0;
 
-// Collapse after the second level
-root.children.forEach(collapse);
+    // Collapse after the second level
+    root.children.forEach(collapse);
 
-update(root);
+    update(root);
 
-// Collapse the node and all it's children
-function collapse(d) {
-  if(d.children) {
-    d._children = d.children
-    d._children.forEach(collapse)
-    d.children = null
-  }
-}
-
-function update(source) {
-
-  // Assigns the x and y position for the nodes
-  var treeData = treemap(root);
-
-  // Compute the new tree layout.
-  var nodes = treeData.descendants(),
-      links = treeData.descendants().slice(1);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d){ d.y = d.depth * 180});
-
-  // ****************** Nodes section ***************************
-
-  // Update the nodes...
-  var node = svg.selectAll('g.node')
-      .data(nodes, function(d) {return d.id || (d.id = ++i); });
-
-  // Enter any new modes at the parent's previous position.
-  var nodeEnter = node.enter().append('g')
-      .attr('class', 'node')
-      .attr("transform", function(d) {
-        return "translate(" + source.y0 + "," + source.x0 + ")";
-    })
-    .on('click', click);
-
-  // Add Circle for the nodes
-  nodeEnter.append('circle')
-      .attr('class', 'node')
-      .attr('r', 1e-6)
-      .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
-      });
-
-  // Add labels for the nodes
-  nodeEnter.append('text')
-      .attr("dy", ".35em")
-      .attr("x", function(d) {
-          return d.children || d._children ? -13 : 13;
-      })
-      .attr("text-anchor", function(d) {
-          return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) { return d.data.name; });
-
-  // UPDATE
-  var nodeUpdate = nodeEnter.merge(node);
-
-  // Transition to the proper position for the node
-  nodeUpdate.transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-        return "translate(" + d.y + "," + d.x + ")";
-     });
-
-  // Update the node attributes and style
-  nodeUpdate.select('circle.node')
-    .attr('r', 10)
-    .style("fill", function(d) {
-        return d._children ? "lightsteelblue" : "#fff";
-    })
-    .attr('cursor', 'pointer');
-
-
-  // Remove any exiting nodes
-  var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) {
-          return "translate(" + source.y + "," + source.x + ")";
-      })
-      .remove();
-
-  // On exit reduce the node circles size to 0
-  nodeExit.select('circle')
-    .attr('r', 1e-6);
-
-  // On exit reduce the opacity of text labels
-  nodeExit.select('text')
-    .style('fill-opacity', 1e-6);
-
-  // ****************** links section ***************************
-
-  // Update the links...
-  var link = svg.selectAll('path.link')
-      .data(links, function(d) { return d.id; });
-
-  // Enter any new links at the parent's previous position.
-  var linkEnter = link.enter().insert('path', "g")
-      .attr("class", "link")
-      .attr('d', function(d){
-        var o = {x: source.x0, y: source.y0}
-        return diagonal(o, o)
-      });
-
-  // UPDATE
-  var linkUpdate = linkEnter.merge(link);
-
-  // Transition back to the parent element position
-  linkUpdate.transition()
-      .duration(duration)
-      .attr('d', function(d){ return diagonal(d, d.parent) });
-
-  // Remove any exiting links
-  var linkExit = link.exit().transition()
-      .duration(duration)
-      .attr('d', function(d) {
-        var o = {x: source.x, y: source.y}
-        return diagonal(o, o)
-      })
-      .remove();
-
-  // Store the old positions for transition.
-  nodes.forEach(function(d){
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-
-  // Creates a curved (diagonal) path from parent to the child nodes
-  function diagonal(s, d) {
-
-    path = `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`
-
-    return path
-  }
-
-  // Toggle children on click.
-  function click(d) {
-    if (d.children) {
-        d._children = d.children;
-        d.children = null;
-      } else {
-        d.children = d._children;
-        d._children = null;
+    // Collapse the node and all it's children
+    function collapse(d) {
+      if(d.children) {
+        d._children = d.children
+        d._children.forEach(collapse)
+        d.children = null
       }
-    update(d);
-  }
-}
+    }
+
+    function update(source) {
+
+      // Assigns the x and y position for the nodes
+      var treeData = treemap(root);
+
+      // Compute the new tree layout.
+      var nodes = treeData.descendants(),
+          links = treeData.descendants().slice(1);
+
+      // Normalize for fixed-depth.
+      nodes.forEach(function(d){ d.y = d.depth * 180});
+
+      // ****************** Nodes section ***************************
+
+      // Update the nodes...
+      var node = treeSvg.selectAll('g.node')
+        .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+      // Enter any new modes at the parent's previous position.
+      var nodeEnter = node.enter().append('g')
+          .attr('class', 'node')
+          .attr("transform", function(d) {
+            return "translate(" + source.y0 + "," + source.x0 + ")";
+        })
+        .on('click', click);
+
+      // Add Circle for the nodes
+      nodeEnter.append('circle')
+          .attr('class', 'node')
+          .attr('r', 1e-6)
+          .style("fill", function(d) {
+              return d._children ? "LawnGreen" : "#fff";
+          });
+
+      // Add labels for the nodes
+      nodeEnter.append('text')
+          .attr("dy", ".35em")
+          .attr("x", function(d) {
+              return d.children || d._children ? -13 : 13;
+          })
+          .attr("text-anchor", function(d) {
+              return d.children || d._children ? "end" : "start";
+          })
+          .text(function(d) { return d.data.name; });
+
+      // UPDATE
+      var nodeUpdate = nodeEnter.merge(node);
+
+      // Transition to the proper position for the node
+      nodeUpdate.transition()
+        .duration(duration)
+        .attr("transform", function(d) {
+            return "translate(" + d.y + "," + d.x + ")";
+         });
+
+      // Update the node attributes and style
+      nodeUpdate.select('circle.node')
+        .attr('r', 10)
+        .style("fill", function(d) {
+            return d._children ? "LawnGreen" : "#fff";
+        })
+        .attr('cursor', 'pointer');
+
+
+      // Remove any exiting nodes
+      var nodeExit = node.exit().transition()
+          .duration(duration)
+          .attr("transform", function(d) {
+              return "translate(" + source.y + "," + source.x + ")";
+          })
+          .remove();
+
+      // On exit reduce the node circles size to 0
+      nodeExit.select('circle')
+        .attr('r', 1e-6);
+
+      // On exit reduce the opacity of text labels
+      nodeExit.select('text')
+        .style('fill-opacity', 1e-6);
+
+      // ****************** links section ***************************
+
+      // Update the links...
+      var link = treeSvg.selectAll('path.link')
+          .data(links, function(d) { return d.id; });
+
+      // Enter any new links at the parent's previous position.
+      var linkEnter = link.enter().insert('path', "g")
+          .attr("class", "link")
+          .attr('d', function(d){
+            var o = {x: source.x0, y: source.y0}
+            return diagonal(o, o)
+          });
+
+      // UPDATE
+      var linkUpdate = linkEnter.merge(link);
+
+      // Transition back to the parent element position
+      linkUpdate.transition()
+          .duration(duration)
+          .attr('d', function(d){ return diagonal(d, d.parent) });
+
+      // Remove any exiting links
+      var linkExit = link.exit().transition()
+          .duration(duration)
+          .attr('d', function(d) {
+            var o = {x: source.x, y: source.y}
+            return diagonal(o, o)
+          })
+          .remove();
+
+      // Store the old positions for transition.
+      nodes.forEach(function(d){
+        d.x0 = d.x;
+        d.y0 = d.y;
+      });
+
+      // Creates a curved (diagonal) path from parent to the child nodes
+      function diagonal(s, d) {
+
+        path = `M ${s.y} ${s.x}
+                C ${(s.y + d.y) / 2} ${s.x},
+                  ${(s.y + d.y) / 2} ${d.x},
+                  ${d.y} ${d.x}`
+
+        return path
+      }
+
+      // Toggle children on click.
+      function click(d) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+          } else {
+            d.children = d._children;
+            d._children = null;
+          }
+        update(d);
+      }
+    }
+
+    // call bar chart function using a player object as an argument
+    makeBarChart(data, playerNumber, 2);
+
+    updateBarchart(data);
 
 };
 
