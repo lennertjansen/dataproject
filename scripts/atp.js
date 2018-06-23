c89a648bbfcd0483a503be144df221d7e04c12c8
Lennert Jansen <lennertjansen@wcw-stud-145-109-46-106.wireless.uva.nl>
2018-06-23 15:37:09 +0200

    autocomplete
    basic version of autocomplete search bar implemented. still working out
    bugs

--- scripts/atp.js
+++ scripts/atp.js
@@ -18,55 +18,7 @@ window.onload = function() {
         .append("p").text("Course: Programming project, spring 2018")
 
     // create empty object for ATP match data
-    playerNames = [];
     playerData = [];
-
-    // function for elimination of duplicate elements in lists
-    Array.prototype.unique = function() {
-
-        return this.filter(function (value, index, self) {
-            return self.indexOf(value) === index;
-
-        });
-
-    };
-
-    //return an array of objects according to key, value, or key and value matching
-    //source: https://gist.github.com/iwek/3924925
-    function getObjects(obj, key, val) {
-        var objects = [];
-        for (var i in obj) {
-            if (!obj.hasOwnProperty(i)) continue;
-            if (typeof obj[i] == 'object') {
-                objects = objects.concat(getObjects(obj[i], key, val));
-            } else
-            //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
-            if (i == key && obj[i] == val || i == key && val == '') { //
-                objects.push(obj);
-            } else if (obj[i] == val && key == ''){
-                //only add if the object is not already in the array
-                if (objects.lastIndexOf(obj) == -1){
-                    objects.push(obj);
-                }
-            }
-        }
-        return objects;
-    }
-
-    //return an array of values that match on a certain key
-    //source: https://gist.github.com/iwek/3924925
-    function getValues(obj, key) {
-        var objects = [];
-        for (var i in obj) {
-            if (!obj.hasOwnProperty(i)) continue;
-            if (typeof obj[i] == 'object') {
-                objects = objects.concat(getValues(obj[i], key));
-            } else if (i == key) {
-                objects.push(obj[i]);
-            }
-        }
-        return objects;
-    };
 
     // retrieve data from JSON file for manipulation with D3
     d3.json("playerdata_new.json", function(data) {
@@ -134,9 +86,22 @@ window.onload = function() {
 
         playerData = data;
 
-        //makeProfile(playerData);
+        playerNames = getValues(data, "name");
 
-        //makeTree(playerData);
+        console.log(autocomplete(document.getElementById("myInput1"), playerNames));
+
+        $(document).ready(function(){
+
+            $('#myInput1').on('submit', function() {
+
+                var value = $(this).val();
+                console.log(value);
+
+
+
+            });
+
+        });
 
         $(document).ready(function(){
             $("#myInput").on("keyup", function() {
@@ -1036,3 +1001,149 @@ function getKeys(obj, val) {
     }
     return objects;
 };
+
+// function for elimination of duplicate elements in lists
+Array.prototype.unique = function() {
+
+    return this.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+
+    });
+
+};
+
+//return an array of objects according to key, value, or key and value matching
+//source: https://gist.github.com/iwek/3924925
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else
+        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+        if (i == key && obj[i] == val || i == key && val == '') { //
+            objects.push(obj);
+        } else if (obj[i] == val && key == ''){
+            //only add if the object is not already in the array
+            if (objects.lastIndexOf(obj) == -1){
+                objects.push(obj);
+            }
+        }
+    }
+    return objects;
+}
+
+//return an array of values that match on a certain key
+//source: https://gist.github.com/iwek/3924925
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+};
+
+// takes in an (incomplete) string argument and array and autocompletes upon selection
+// source: https://www.w3schools.com/howto/howto_js_autocomplete.asp
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
